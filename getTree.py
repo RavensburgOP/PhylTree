fe2b0e6d3dd74fb23f452a36f27dfb7da101ec25
Ravnen <christian.ravnen@gmail.com>
2015-09-24 18:35:12 +0200

    Removed the use of regex in line 131-133 as the Biopython parser already takes care of it, and the current implementation was creating errors.

--- getTree.py
+++ getTree.py
@@ -1,23 +1,41 @@
 from Bio.Phylo.Applications import _Fasttree
 import Bio.Phylo as Phylo
 from Bio import SeqIO
+import networkx, pylab
+import numpy as np
 
 fasttree_exe = "./FastTree"
 file = "glob"
 
+# Load all protein names
+handle = open(file+".ali", "rU")
+strucParse = struc = SeqIO.parse(handle, "pir")
+protein_names = [i.id for i in struc]
+
+# Convert file so fasttree can use it
 sequences = SeqIO.convert(file+".ali", "pir", file+".fasta", "fasta")
 
 cmd = _Fasttree.FastTreeCommandline(fasttree_exe, input=file+".fasta", out='ExampleTree.tree')
 
-
 tree = Phylo.read('ExampleTree.tree', "newick")
 
-for i in tree.find_clades():
-    # Is node before leaves
-    if i.is_preterminal():
-        # Can't use len for iter. Needs list.
-        sub_tree = list(i.find_clades())
-        if len(sub_tree) > 2:
-            j1, j2, j3 = sub_tree
-            # Distance between two leaves == similarity
-            print j1.distance(j2, j3)
+cherryX = []
+cherryY = []
+count = 0
+
+# # Find leaf pairs
+# for i in tree.find_clades():
+#     # Is node before leaves
+#     if i.is_preterminal():
+#         count += 1
+#         # Can't use len for iter. Needs list.
+#         sub_tree = list(i.find_clades())
+#         if len(sub_tree) > 2:
+#             j1, j2, j3 = sub_tree
+#             cherryY.append(j2, j3)
+#             cherryX.append(j3, j2)
+
+net = Phylo.to_networkx(tree)
+adj_mat = networkx.adjacency_matrix(net)
+print adj_mat
+np.savetxt("test.csv", adj_mat.todense())
